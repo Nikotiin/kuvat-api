531bb5124bc749e406761462c5276fba6491315c
Niko Riipiranta <nikorii@gmail.com>
2020-07-02 23:20:24 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kuvat-api",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     author="Niko Riipiranta",
     description="Kuvat.fi API",
